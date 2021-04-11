f9a56569a4bf1b5e51dc747bdb148e8531b3da83
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool>
2021-04-11 21:56:43 +0300

    added function on my output button

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
-var results = function() {
-    let daysOfTheWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let maleNames = [
-      "Kwasi",
-      "Kwadwo",
-      "Kwabena",
-      "Kwaku",
-      "Yaw",
-      "Kofi",
-      "Kwame",
-    ];
-    let femaleNames = [
-     "Akosua",
-     " Adwoa",
-     "Abenaa",
-     "Akua",
-     "Yaa",
-     "Afua",
-     "Ama",
-    ];
-    
+var results = function () {
+  let daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let maleNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  let femaleNames = [
+    "Akosua",
+    " Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+  ];
+  let century = parseInt(document.getElementById("century").value);
+  let year = parseInt(document.getElementById("year").value);
+  let month = parseInt(document.getElementById("month").value);
+  let day = parseInt(document.getElementById("day").value);
+  let daysOfBirth = new Date(((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + day) % 7
+  if (month == "0" && year == "0" && day == "0") {
+    alert("Please Enter valid record ");
+    return false;
+  }
+}
